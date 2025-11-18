11450b9f04f8da94b97cac9490331baec4f99de6
fcunnane <franciscunnane3@gmail.com>
2025-11-18 00:46:09 -0500

    Create program_signaltap.sh

---
+++ fpga/program_signaltap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo
+echo "Flashing SignalTap.sof (hands-free collapse demo)"
+echo
+quartus_pgm -c 1 -m jtag -o "p;SignalTap.sof"
+echo
+echo "Done! Open SignalTap_demo.stp and click Run Analysis."
+read -p "Press Enter to exit"
